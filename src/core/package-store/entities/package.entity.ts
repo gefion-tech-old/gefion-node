2575cfc35eaf3d8a05c7a8eb0204eff709722f5f
6y6eH4uk <masterlandos@gmail.com>
2021-07-30 04:00:53 +0300

    Типы модуля package-store

--- src/core/package-store/entities/package.entity.ts
+++ src/core/package-store/entities/package.entity.ts
@@ -1,6 +1,7 @@
 import { injectable } from 'inversify'
 import { Entity, PrimaryColumn, Column, OneToMany } from 'typeorm'
 import { PackageTag } from './package-tag.entity'
+import { PackageType } from '../package-store.types'
 
 @injectable()
 @Entity()
@@ -14,7 +15,7 @@ export class Package {
     @Column({
         nullable: false
     })
-    type: 'constant' | 'temporary';
+    type: PackageType;
 
     @OneToMany(() => PackageTag, tag => tag.package, {
         cascade: true,
--- src/core/package-store/package-store.types.ts
+++ src/core/package-store/package-store.types.ts
@@ -1,10 +1,51 @@
 export const PACKAGE_STORE_SYMBOL = {
     PackageStoreConfig: Symbol.for('PackageStoreConfig'),
     PackageEntity: Symbol.for('PackageEntity'),
-    PackageTagEntity: Symbol.for('PackageTagEntity')
+    PackageTagEntity: Symbol.for('PackageTagEntity'),
+    PackageRepository: Symbol.for('PackageRepository'),
+    PackageTagRepository: Symbol.for('PackageTagRepository'),
+    PackageStoreService: Symbol.for('PackageStoreService')
 }
 
 export type PackageStoreConfig = {
     // Путь к каталогу пакетов
     readonly packageDir: string
+}
+
+export type OptionsInstallPackage = {
+    gitPath: string,
+    tag: Tag,
+    type: PackageType
+    credential?: GitCredential
+}
+
+export type Package = {
+    // Тип пакета (постоянный или временный)
+    readonly type: PackageType
+    // Путь к удаленному git репозиторию
+    readonly gitPath: string
+    // Путь в каталоге файловой системы к пакету
+    readonly path: string
+    // Список установленных тегов
+    readonly installedTags: Array<PackageTag>
+    // Список всех доступных для установки тегов
+    readonly allAvailableTags: Array<Tag>
+}
+
+export type Packages = Array<Package>
+
+export type PackageTag = {
+    // Название тега
+    readonly name: Tag
+    // Путь к каталогу в файловой системе с версией
+    readonly path: string
+}
+
+export type Tag = string
+
+export type PackageType = 'constant' | 'temporary'
+
+export type GitCredential = {
+    readonly username: string,
+    readonly password: string
 }
