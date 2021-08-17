c90208de666234a91f060a0fa79f7f54bad34cc8
6y6eH4uk <adkins-skot@mail.ru>
2021-08-17 20:17:17 +0300

    Добавил тест на утечку памяти в AtomicModule

--- src/core/atomic/atomic.spec.ts
+++ src/core/atomic/atomic.spec.ts
@@ -1,6 +1,8 @@
 import { getContainer } from '../../inversify.config'
 import { ATOMIC_SYMBOL } from './atomic.types'
 import { IAtomicService } from './atomic.interface'
+import { getUsedArrayBuffers } from '../../utils/gc'
+import { Buffer } from 'buffer'
 
 describe('Сервис атомарных операций', () => {
 
@@ -32,6 +34,39 @@ describe('Сервис атомарных операций', () => {
         await atomicService.unlock(operation)
 
         expect(await atomicService.check(operation)).toBe(false)
+    })
+
+    it('Ссылки на объект опций успешно освобождаются в методах lock и check #gc', async () => {
+        const container = await getContainer()
+        const atomicService = container
+            .get<IAtomicService>(ATOMIC_SYMBOL.AtomicService)
+        const operation = 'Operation3'
+
+        const bufferSize = 30 * 1024 * 1024
+        
+        await expect(await (async (): Promise<number> => {
+            const buffer = Buffer.allocUnsafe(bufferSize)
+            const usedMemory = getUsedArrayBuffers()
+
+            await atomicService.lock(operation, ({
+                retries: 1,
+                ef: buffer
+            } as any))
+
+            return usedMemory
+        })()).toBeGreaterThan(getUsedArrayBuffers() + bufferSize * 0.9)
+
+        await expect(await (async (): Promise<number> => {
+            const buffer = Buffer.allocUnsafe(bufferSize)
+            const usedMemory = getUsedArrayBuffers()
+
+            await atomicService.check(operation, ({
+                retries: 1,
+                ef: buffer
+            } as any))
+
+            return usedMemory
+        })()).toBeGreaterThan(getUsedArrayBuffers() + bufferSize * 0.9)
     })
 
 })
