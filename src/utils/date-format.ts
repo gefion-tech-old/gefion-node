f926da91aa1519bebe8a5509f0535509e40994a2
6y6eH4uk <adkins-skot@mail.ru>
2021-09-01 17:47:10 +0300

    Описание date-format в utils

--- src/utils/date-format.ts
+++ src/utils/date-format.ts
@@ -1,5 +1,8 @@
 import format from 'dateformat'
 
+/**
+ * Получить дату в формате, который понимает sqlite
+ */
 export function getSqliteDateFormat(date: Date) {
     return format(date, 'yyyy-mm-dd HH:MM:ss.l', true)
 }
