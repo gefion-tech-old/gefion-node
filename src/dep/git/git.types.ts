8d61e6ae23bb3974418d9d43abe737dc85a8aa7d
6y6eH4uk <adkins-skot@mail.ru>
2021-09-01 14:02:29 +0300

    Удалил ненужные модули

--- src/dep/git/git.types.ts
+++
@@ -1,9 +0,0 @@
-export const GIT_SYMBOL = {
-    GitConfig: Symbol('GitConfig'),
-    Git: Symbol('Git')
-}
-
-export type GitConfig = {
-    // Максимальное количество параллельно порождающихся процессов
-    maxConcurrentProcesses: number
-}
