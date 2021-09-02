2e40a608215d44ed275b6f43f6228db3d8adb4de
6y6eH4uk <adkins-skot@mail.ru>
2021-09-02 13:32:24 +0300

    Изменение TypeOrmModule

--- src/core/typeorm/typeorm.types.ts
+++ src/core/typeorm/typeorm.types.ts
@@ -1,6 +1,5 @@
 export const TYPEORM_SYMBOL = {
     TypeOrmConnectionAppConfig: Symbol('TypeOrmConnectionConfigApp'),
     TypeOrmConnectionApp: Symbol('TypeOrmConnectionApp'),
-    TypeOrmAppEntity: Symbol('TypeOrmAppEntity'),
-    TypeOrmAppRepository: Symbol('TypeOrmAppRepository')
+    TypeOrmAppEntity: Symbol('TypeOrmAppEntity')
 }
